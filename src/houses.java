33cb796f8eac4bf87b59e4a77040c2ab0e56d63b
league <league@wts10.attlocal.net>
2018-10-24 18:25:09 -0700

    houses

--- src/houses.java
+++ src/houses.java
@@ -1,18 +1,41 @@
 import org.jointheleague.graphical.robot.Robot;
 
 public class houses {
+	static Robot Happyday=new Robot();
 public static void main(String[] args) {
-	Robot Happyday=new Robot();
-	Happyday.setX(1500);
-	Happyday.setY(20);
+	
+    Happyday.setX(100);
+	Happyday.setY(200);
 	Happyday.setPenWidth(10);
 	Happyday.penDown();
 	Happyday.setSpeed(10);
 	Happyday.setPenColor(85,85,204);
-	Happyday.move(120);
-	Happyday.turn(90);
-	Happyday.move(120);
-	Happyday.turn(90);
+	for(int i=0;i<10;i++) {
+		draw(small);
 	}
 }
+	static void draw(String height) {
+	int height2;
+		if (height=="small") {
+			height2=60;
+		}
+		if (height=="medium") {
+			height2=120;
+		}
+		if (height=="large") {
+			height2=250;
+		}
+	Happyday.move(120);
+	Happyday.turn(90);
+	Happyday.move(20);
+	Happyday.turn(90);
+	Happyday.move(120);
+	Happyday.turn(90);
+	Happyday.move(20);
+	Happyday.turn(90);
+	
+	}
+	
+	
 
+}
