baa06a6edc3afcdfbbd878e425ea1c03cea0ee16
league <league@wts5.attlocal.net>
2018-10-31 18:18:00 -0700

    houses and Jframe

--- src/houses.java
+++ src/houses.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class houses {
@@ -10,12 +12,15 @@ public static void main(String[] args) {
 	Happyday.penDown();
 	Happyday.setSpeed(10);
 	Happyday.setPenColor(85,85,204);
-	for(int i=0;i<10;i++) {
-		draw(small);
+	for(int i=0;i<5;i++) {
+		draw("small",Color.red);
 	}
 }
-	static void draw(String height) {
-	int height2;
+
+
+	static void draw(String height,Color color) {
+	int height2=60;
+
 		if (height=="small") {
 			height2=60;
 		}
@@ -25,17 +30,39 @@ public static void main(String[] args) {
 		if (height=="large") {
 			height2=250;
 		}
-	Happyday.move(120);
-	Happyday.turn(90);
-	Happyday.move(20);
-	Happyday.turn(90);
-	Happyday.move(120);
-	Happyday.turn(90);
-	Happyday.move(20);
-	Happyday.turn(90);
-	
+		
+
+	pointyroof(height2,color);
+	flatroof(height2,color);
 	}
-	
-	
+	static void pointyroof(int height,Color color) {
+		Happyday.setPenColor(color);	
+		Happyday.move(120);
+		Happyday.turn(90);
+		Happyday.turn(-45);
+		Happyday.move(60);
+		Happyday.move(20);
+		Happyday.turn(90);
+		Happyday.move(60);
+		Happyday.turn(90);
+		Happyday.turn(45);
+		Happyday.turn(-90);
+		Happyday.move(120);
+		Happyday.turn(-90);
+		Happyday.move(20);
+		Happyday.turn(-90);
+		
+	}
+static void flatroof(int height,Color color) {
+	Happyday.setPenColor(color);	
+	Happyday.move(height);
+	Happyday.turn(90);
+	Happyday.move(60);
+	Happyday.turn(90);
+	Happyday.move(height);
+	Happyday.turn(-90);
+	Happyday.move(20);
+	Happyday.turn(-90);
+	}
 
 }
